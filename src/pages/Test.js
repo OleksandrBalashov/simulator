499228f70ffb88a5708008c0aed58d9577734db9
Maksym <74206337+Maxlemberg@users.noreply.github.com>
2021-06-08 00:15:41 +0300

    testPage

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import TestLogo from '../images/test.svg';
+import styles from './styles/Test.module.css';
 
 const Test = () => {
+  const handleClick = e => {
+    console.log('🥇 new Page');
+  };
   return (
-    <div>
-      <h1>Опросник</h1>
+    <div className={styles.boxTest}>
+      <h1 className={styles.title}>
+        Пройдите тест на определение профориентации
+      </h1>
+      <img src={TestLogo} alt="test" className={styles.testLogo} />
+      <p className={styles.text}>
+        Существует несколько тысяч различных специальностей, и у каждой есть
+        своя специфика. Чтобы определиться, что подходит именно вам - пройдите
+        наш тест на профориентацию.
+      </p>
+      <button type="button" className={styles.btn} onClick={handleClick}>
+        Начать
+      </button>
     </div>
   );
 };
